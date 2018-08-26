b3f5d026c833463f64bd9e76406856ba30548b63
uopsdod <uopsdod@gmail.com>
2018-08-26 22:37:07 +0800

    add default logger

--- AutoLogin_1point3acres/src/main/java/com/amazonaws/lambda/demo/util/DailyRewardUtil.java
+++ AutoLogin_1point3acres/src/main/java/com/amazonaws/lambda/demo/util/DailyRewardUtil.java
@@ -1,7 +1,15 @@
 package com.amazonaws.lambda.demo.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class DailyRewardUtil {
 	private static BaseLogger logger;
+	
+	static {
+		Logger fileLogger = LogManager.getLogger("util.fileLogger");
+		setLogger(new MyLog4jLogger(fileLogger));
+	}
 
 	public static BaseLogger getLogger() {
 		return logger;
