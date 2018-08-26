c844bb58f60898cc36608ef94bab1a2d532bc698
uopsdod <uopsdod@gmail.com>
2018-08-26 22:36:17 +0800

    MyLambdaLogger: add overload log method for throwable type

--- AutoLogin_1point3acres/src/main/java/com/amazonaws/lambda/demo/util/MyLambdaLogger.java
+++ AutoLogin_1point3acres/src/main/java/com/amazonaws/lambda/demo/util/MyLambdaLogger.java
@@ -1,5 +1,7 @@
 package com.amazonaws.lambda.demo.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.logging.Level;
 
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -17,6 +19,18 @@ public class MyLambdaLogger implements BaseLogger{
 	@Override
 	public void log(Level level, String msg) {
 		this.logger.log(level.getName() + " - " + msg);
+	}
+
+
+
+	@Override
+	public void log(Level level, Throwable error) {
+		/** get the whole stack trace of error **/
+	    StringWriter trace = new StringWriter();
+	    error.printStackTrace(new PrintWriter(trace));
+	    String eMsg = trace.toString();
+	    
+	    this.log(level, eMsg);
 	}
 
 }
