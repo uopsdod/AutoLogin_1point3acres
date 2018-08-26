74d2880f663d884fcfb6f78e1d665e9381e59392
uopsdod <uopsdod@gmail.com>
2018-08-26 15:24:24 +0800

    add dailySignIn test

---
+++ AutoLogin_1point3acres/src/test/java/com/amazonaws/lambda/demo/daily_reward/DailySignInTest.java
@@ -0,0 +1,55 @@
+package com.amazonaws.lambda.demo.daily_reward;
+
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.amazonaws.lambda.demo.daily_reward.DailyRewardInput;
+import com.amazonaws.lambda.demo.daily_reward.DailyRewardService;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+
+/**
+ * test daily sign in
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DailySignInTest {
+
+    @Before
+    public void setUp() throws IOException {
+    	
+    }
+
+    @Test
+    public void dailySignIn() {
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+        DailyRewardService autoLoginService = new DailyRewardService();
+    	boolean isLogined = autoLoginService.login(username, password);
+    	
+    	Assert.assertEquals(isLogined, true);
+    	
+		String dailySignInFormHash = autoLoginService.getDailySignInFormHash();
+		boolean isDailySignInDone = false;
+		if (!dailySignInFormHash.isEmpty()) {
+			isDailySignInDone = autoLoginService.dailySignIn(dailySignInFormHash);
+			Assert.assertEquals(isDailySignInDone, true);
+		}
+		Assert.assertEquals(isDailySignInDone, false);
+		
+    }
+    
+}
